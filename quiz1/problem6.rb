228e599a459c7af6167d77faadc8087d0a423ff0
Marc Davis <marcbobson@gmail.com>
2015-08-04 13:54:25 +0000

    powerful

---
+++ quiz1/problem6.rb
@@ -0,0 +1,16 @@
+famous_words = "and seven years ago..."
+
+puts "four score " + famous_words
+
+beginning="four score "
+
+beginning << famous_words
+puts beginning
+
+beginning="four score "
+beginning.concat(famous_words)
+puts beginning
+
+
+ 
+
