bcaa127ba8913bc0cb53f828188eed440efac524
Marc Davis <marcbobson@gmail.com>
2015-08-07 03:09:44 +0000

    wow

---
+++ quiz2/problem4.rb
@@ -0,0 +1,12 @@
+md = "The Munsters are creepy in a good way."
+
+puts "Capitalize: " + md.capitalize
+
+md = "The Munsters are creepy in a good way."
+puts "Swapcase: " + md.swapcase
+
+md = "The Munsters are creepy in a good way."
+puts "Downcase: " + md.downcase
+
+md = "The Munsters are creepy in a good way."
+puts "Upcase: " + md.upcase
