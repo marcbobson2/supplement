9691132e8b8adc55d8449c3cf96aaddcc6d3d95c
Marc Davis <marcbobson@gmail.com>
2015-08-10 23:36:49 +0000

    wow

---
+++ quiz3/problem1.rb
@@ -0,0 +1,7 @@
+flintstones = ["Fred", "Barney", "Wilma", "Betty", "BamBam", "Pebbles"]
+
+flintstones_easy=%w(Fred Barney Wilma Betty BamBam Pebbles)
+
+p flintstones_easy
+
+
---
+++ quiz3/problem3.rb
@@ -0,0 +1,9 @@
+
+flintstones_easy=%w(Fred Barney Wilma Betty BamBam Pebbles)
+
+flintstones_easy.push("Dino")
+
+flintstones_easy.insert(-1,"Pet1","Pet2","Pet3")
+
+p flintstones_easy
+
