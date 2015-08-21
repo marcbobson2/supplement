779d8a242788391302c53564af6ea43bd4d4dfc3
Marc Davis <marcbobson@gmail.com>
2015-08-21 02:38:36 +0000

    wow

---
+++ quiz2int/problem6.rb
@@ -0,0 +1,33 @@
+munsters = {
+  "Herman" => { "age" => 32, "gender" => "male" },
+  "Lily" => { "age" => 30, "gender" => "female" },
+  "Grandpa" => { "age" => 402, "gender" => "male" },
+  "Eddie" => { "age" => 10, "gender" => "male" },
+  "Marilyn" => { "age" => 23, "gender" => "female"}
+}
+
+def mess_with_demographics(demo_hash)
+  demo_hash.values.each do |family_member|
+    family_member["age"] += 42
+    family_member["gender"] = "other"
+  end
+end
+
+mess_with_demographics(munsters)
+p munsters
+
+
+animal_array=[5,6,7,8]
+
+def mess_with_animals(demo_array)
+# demo_array[1] +=20
+demo_array.each do |value|
+  p value
+  value +=20
+end
+
+
+end
+
+mess_with_animals(animal_array)
+p animal_array
